eb5c6d2cc262456117a0b182f2a949760821ec3f
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-02-06 22:47:35 +0700

    Initiate swiftUI project

---
+++ HabitCo/HabitCo/HabitCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitCoApp.swift
+//  HabitCo
+//
+//  Created by Figo Alessandro Lehman on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
