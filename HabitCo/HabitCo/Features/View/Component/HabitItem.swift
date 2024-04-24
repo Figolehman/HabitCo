82ea1ff1cc3913d5178e33d621c8717151eaa825
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-04-25 03:00:24 +0700

    feat : add swipe gesture for habit item to reveal undo button

--- HabitCo/HabitCo/Features/View/Component/HabitItem.swift
+++ HabitCo/HabitCo/Features/View/Component/HabitItem.swift
@@ -13,6 +13,9 @@ enum HabitType {
 }
 
 struct HabitItem: View {
+    @State private var offset: CGFloat = 0
+    @State private var firstOffset: CGFloat = 0
+    
     let progressSize: CGFloat = 50
     let habitType: HabitType
     let habitName: String
@@ -32,162 +35,126 @@ struct HabitItem: View {
     }
     
     var body: some View {
-        HStack (spacing: 4) {
-            Button {
-                navigate()
-            } label: {
-                HStack (spacing: 16){
-                    Rectangle()
-                        .foregroundColor(.yellow)
-                        .frame(width: 20, height: 20)
-                        .cornerRadius(4)
-                    
-                    Text("\(habitName)")
-                        .foregroundColor(Color.getAppColor(.neutral3))
-                    
-                    Spacer()
-                }
-                .padding(.horizontal, 16)
-                .frame(width: .infinity, height: .getResponsiveHeight(80))
-                .background(
-                    Color.getAppColor(.primary)
-                )
-            }
-            
-//            .cornerRadius(12)
-            
-            Button {
-                action()
-            } label: {
-                Group {
-                    switch habitType {
-                    case .regular:
-                        VStack {
-                            Image(systemName: "timer")
-                                .font(.title3)
-                                .padding(2)
+        HStack(spacing: 16) {
+            HStack (spacing: .getResponsiveWidth(4)) {
+                Button {
+                    navigate()
+                } label: {
+                    HStack (spacing: 16){
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .frame(width: 20, height: 20)
+                            .cornerRadius(4)
                         
-                            Text("\(progress) Session")
-                                .font(.caption2)
-                        }
-                        .padding(.horizontal, 12)
-                    case .pomodoro:
-                        ZStack {
-                            
-                            Circle()
-                                .foregroundColor(.getAppColor(.primary))
-                                .frame(width: progressSize, height: progressSize)
-                            
-                            Circle()
-                                .stroke(Color.getAppColor(.primary2), lineWidth: 5)
-                                .frame(width: progressSize, height: progressSize)
-                            
-                            
-                            Circle()
-                                .trim(from: 0.0, to: CGFloat(fraction))
-                                .stroke(Color.getAppColor(.neutral), lineWidth: 5)
-                                .frame(width: progressSize, height: progressSize)
-                                .rotationEffect(.degrees(-90))
-                            
-                            Text("\(progress)")
-                                .foregroundColor(.getAppColor(.neutral3))
-                        }
-                        .padding(.horizontal, 12)
+                        Text("\(habitName)")
+                            .foregroundColor(Color.getAppColor(.neutral3))
+                        
+                        Spacer()
                     }
+                    .padding(.horizontal, 16)
+                    .frame(width: .getResponsiveWidth(265), height: .getResponsiveHeight(80))
+                    .background(
+                        Color.getAppColor(.primary)
+                    )
                 }
-                .foregroundColor(.getAppColor(.neutral3))
-                .frame(width: .infinity, height: .getResponsiveHeight(80))
-                .background(
-                    Color.getAppColor(.primary)
-                )
+                
+    //            .cornerRadius(12)
+                
+                    Button {
+                        action()
+                    } label: {
+                        Group {
+                            switch habitType {
+                            case .regular:
+                                VStack {
+                                    Image(systemName: "timer")
+                                        .font(.title3)
+                                        .padding(2)
+                                
+                                    Text("\(progress) Session")
+                                        .font(.caption2)
+                                }
+                                .padding(.horizontal, 12)
+                            case .pomodoro:
+                                ZStack {
+                                    
+                                    Circle()
+                                        .foregroundColor(.getAppColor(.primary))
+                                        .frame(width: progressSize, height: progressSize)
+                                    
+                                    Circle()
+                                        .stroke(Color.getAppColor(.primary2), lineWidth: 5)
+                                        .frame(width: progressSize, height: progressSize)
+                                    
+                                    
+                                    Circle()
+                                        .trim(from: 0.0, to: CGFloat(fraction))
+                                        .stroke(Color.getAppColor(.neutral), lineWidth: 5)
+                                        .frame(width: progressSize, height: progressSize)
+                                        .rotationEffect(.degrees(-90))
+                                    
+                                    Text("\(progress)")
+                                        .foregroundColor(.getAppColor(.neutral3))
+                                }
+                                .padding(.horizontal, 12)
+                            }
+                        }
+                        .foregroundColor(.getAppColor(.neutral3))
+                        .frame(width: .getResponsiveWidth(76), height: .getResponsiveHeight(80))
+                        .background(
+                            Color.getAppColor(.primary)
+                        )
 
+                    }
+                
             }
+            .mask(
+                Rectangle()
+                    .cornerRadius(12)
+            )
             
-                        
+            
+            Button {
+                
+            } label: {
+                Text("Undo")
+                    .foregroundColor(Color.getAppColor(.neutral3))
+                    .frame(width: .getResponsiveWidth(116), height: .getResponsiveHeight(80))
+                    .background(
+                        Color.getAppColor(.secondary)
+                    )
+                    .cornerRadius(12)
+            }
         }
-        .frame(width: .getResponsiveWidth(345), height: .getResponsiveHeight(80))
+        .offset(x: offset + .getResponsiveWidth(66))
         .mask(
             Rectangle()
-                .cornerRadius(12)
+                .frame(width: .getResponsiveWidth(345), height: .getResponsiveHeight(80))
         )
-    }
-    
-    var bod: some View {
-        HStack {
-            Group {
-                Rectangle()
-                    .foregroundColor(.yellow)
-                    .frame(width: 20, height: 20)
-                    .cornerRadius(4)
-                    .padding(.trailing, 16)
-                
-                Text("\(habitName)")
-                
-                Spacer()
-            }
-            .onTapGesture {
-                // view habit detail
-            }
-            
-            Divider()
-                .frame(width: 1, height: .getResponsiveHeight(62))
-                .overlay (
-                    Color.getAppColor(.neutral3)
-                )
-            
-            
-            switch habitType {
-            case .regular:
-                VStack {
-                    Image(systemName: "timer")
-                        .font(.title3)
-                        .padding(2)
-                
-                    Text("\(progress) Session")
-                        .font(.caption2)
+        .highPriorityGesture(
+            DragGesture()
+                .onChanged { value in
+                    let translation = value.translation.width
+                    self.offset = min(0, self.firstOffset + translation)
+//                    self.offset = translation
                 }
-                .padding(6)
-            case .pomodoro:
-                ZStack {
-                    
-                    Circle()
-                        .foregroundColor(.getAppColor(.primary))
-                        .frame(width: progressSize, height: progressSize)
-                    
-                    Circle()
-                        .stroke(Color.getAppColor(.primary2), lineWidth: 5)
-                        .frame(width: progressSize, height: progressSize)
-                    
-                    
-                    Circle()
-                        .trim(from: 0.0, to: CGFloat(fraction))
-                        .stroke(Color.getAppColor(.neutral), lineWidth: 5)
-                        .frame(width: progressSize, height: progressSize)
-                        .rotationEffect(.degrees(-90))
-                    
-                    Text("\(progress)")
-                        .foregroundColor(.getAppColor(.neutral3))
+                .onEnded { value in
+                    if self.offset <= .getResponsiveWidth(-70) {
+                        self.offset = -133
+                    } else {
+                        self.offset = 0
+                    }
+                    self.firstOffset = self.offset
                 }
-                .padding(.horizontal, 6)
-                .onTapGesture {
-                    
-                }
-            }
-            
-            
-        }
-        .foregroundColor(.getAppColor(.neutral3))
-        .padding(.horizontal, 12)
-        .frame(width: .getResponsiveWidth(345), height: .getResponsiveHeight(80), alignment: .center)
-        .background(Color.getAppColor(.primary))
-        .cornerRadius(12)
-        .shadow(color: Color(red: 0.09, green: 0.09, blue: 0.09).opacity(0.1), radius: 8, x: 0, y: 4)
+        )
     }
 }
 
 #Preview {
-    VStack {
-        HabitItem(habitType: .regular, habitName: "test", fraction: 0.5)
-        HabitItem(habitType: .pomodoro, habitName: "test", fraction: 0.5)
+    NavigationView {
+        VStack {
+            HabitItem(habitType: .regular, habitName: "test", fraction: 0.5)
+            HabitItem(habitType: .pomodoro, habitName: "test", fraction: 0.5)
+        }
     }
 }
