a9d7f8859172a1657e2c1dd5a14ae88001a09366
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-04-25 11:31:17 +0700

    fix : return content view to previous version

--- HabitCo/HabitCo/Features/View/ContentView.swift
+++ HabitCo/HabitCo/Features/View/ContentView.swift
@@ -9,157 +9,85 @@ import SwiftUI
 import AuthenticationServices
 
 struct ContentView: View {
-    @State var titles = ["Cell #1", "Cell #2", "Cell #3", "Cell #4"]
-
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(spacing: 0) {
-                    ForEach(self.titles, id: \.self) { title in
-                        HStack {
-                            Text(title)
-                            Spacer()
-                        }
-                        .padding()
-                        .contentShape(Rectangle())
-                        .swipeButtons([
-                            CustomSwipeButton(
-                                image: Image(systemName: "plus"),
-                                title: "Duplicate",
-                                color: .blue,
-                                action: { self.titles.append("\(title) (2)") }
-                            ),
-                            CustomSwipeButton(
-                                image: Image(systemName: "trash"),
-                                title: "Delete",
-                                color: .red,
-                                action: { self.titles.remove(at: self.titles.firstIndex(of: title)!) }
-                            )
-                        ])
-                    }
-                }
-                .animation(.default, value: self.titles)
+    @State private var selectedDate = Date()
+    
+    @State private var isRepeatOn = false
+    
+    @State private var isRepeatFolded = true
+    
+    @State private var repeatDate: Set<RepeatDay> = []
+    
+    var repeatWeekdays: [Int] {
+        get {
+            let weekdays = repeatDate.map {
+                $0.weekday
             }
-            .navigationTitle(Text("Custom swipe action"))
+            return weekdays
         }
     }
-}
-
-struct CustomSwipeButton {
-    let image: Image?
-    let title: String?
-    let color: Color
-    let action: () -> Void
-}
-
-struct SwipeButtonsModifier: ViewModifier {
-    @State private var position: CGFloat = 0
-    @State private var lastPosition: CGFloat = 0
-    @State private var swipeShouldPublishNotification = true
-
-    let buttons: [CustomSwipeButton]
-
-    private let notificationName = Notification.Name("customSwipeDidStart")
-    private let buttonWidth: CGFloat = 70
-    private let animation = Animation.spring(response: 0.3, dampingFraction: 0.65, blendDuration: 0.25)
-
-    private var count: CGFloat {
-        CGFloat(self.buttons.count)
-    }
-
-    func body(content: Content) -> some View {
-        ZStack {
-            HStack(spacing: 0) {
-                content
-                    .hidden()
-
-                ForEach(Array(zip(self.buttons, self.buttons.indices)), id: \.0.title) { button, idx in
-                    let width = max(0, -self.position / self.count)
-
-                    Button {
-                        self.dismiss()
-
-                        button.action()
-                    } label: {
-                        VStack {
-                            if let image = button.image {
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 20, height: 20)
-                            }
-
-                            if let title = button.title {
-                                Text(title)
-                                    .fixedSize()
-                                    .font(.footnote)
-                            }
-                        }
-                        .foregroundColor(.white)
-                        .frame(width: width)
-                        .frame(maxHeight: .infinity)
-                        .background(button.color)
-                    }
-                }
-            }
-
-            content
-                .offset(x: self.position)
-                .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            if self.swipeShouldPublishNotification {
-                                NotificationCenter.default.post(Notification(name: self.notificationName))
-
-                                self.swipeShouldPublishNotification = false
-                            }
-
-                            if value.translation.width < 0 || self.lastPosition < 0 {
-                                self.position = self.lastPosition + value.translation.width
-                            }
-                        }
-                        .onEnded { value in
-                            self.swipeShouldPublishNotification = true
-
-                            if value.translation.width < 0 && abs(value.translation.width + self.lastPosition) > 20 * self.count {
-                                let fixedWidth = -self.buttonWidth * self.count
-
-                                withAnimation(self.animation) {
-                                    self.position = fixedWidth
-                                    self.lastPosition = fixedWidth
+    //    @AppStorage("notificationTimeString") var notificationTimeString = ""
+    //    @AppStorage("currentProgress") var currentProgress: Int = 3
+    
+    let notify = NotificationHandler()
+    
+    var body: some View {
+        VStack {
+            CardView {
+                VStack (spacing: 12) {
+                    HStack {
+                        Text("Repeat")
+                        Spacer()
+                        AppButton(label: "\(repeatDate.getRepeatLabel())", sizeType: .select) {
+                            if isRepeatFolded {
+                                withAnimation {
+                                    isRepeatFolded = false
+                                    isRepeatOn = true
                                 }
                             } else {
-                                self.dismiss()
+                                withAnimation {
+                                    isRepeatFolded = true
+                                }
+                            }
+                            
+                        }
+                    }
+                    if !isRepeatFolded {
+                        Toggle("Set repeat", isOn: $isRepeatOn.animation())
+                            .toggleStyle(SwitchToggleStyle(tint: .getAppColor(.primary)))
+                        if isRepeatOn {
+                            HStack {
+                                ForEach(RepeatDay.allCases, id: \.self) { day in
+                                    RepeatButton(repeatDays: $repeatDate, day: day)
+                                }
                             }
                         }
-                )
-                .onReceive(NotificationCenter.default.publisher(for: self.notificationName)) { _ in
-                    self.dismiss()
+                        Spacer()
+                        Button("Replace"){
+                            notify.replaceNotification(withIdentifier: "journal")
+                        }
+                        
+                        DatePicker("Notification Time", selection: Binding(get: {
+                            Date()
+                        }, set: { date in
+                            notify.askPermission()
+                            notify.sendNotification(date: date, weekdays: repeatWeekdays, title: "tes", body: "tes", withIdentifier: "journal")
+                        }), displayedComponents: [.hourAndMinute]
+                        )
+                        .datePickerStyle(.wheel)
+                        Button("notify 5 seconds after") {
+                            //                notify.sendNotification(date: Date(), type: "time", timeInterval: 5, title: "TEST", body: "your current progress is")
+                        }
+                        Spacer()
+                        Button("Request permission") {
+                            notify.askPermission()
+                        }
+                    }
+                    
                 }
-        }
-    }
-
-    private func dismiss() {
-        withAnimation(self.animation) {
-            self.position = 0
-            self.lastPosition = 0
+            }
         }
     }
 }
-
-extension View {
-    func swipeButtons(_ buttons: [CustomSwipeButton]) -> some View {
-        self.modifier(SwipeButtonsModifier(buttons: buttons))
-    }
-}
-
-//struct DateHelper {
-//    static let dateFormatter: DateFormatter = {
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "HH:mm"
-//        return formatter
-//    }()
-//}
 
 #Preview {
     ContentView()
