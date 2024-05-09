9590279973c98562088154360dd69c1cd3945cee
GeraldyK <geraldy.gk@gmail.com>
2024-05-09 14:50:36 +0700

    Feat: Add widget viewModel

---
+++ HabitCo/HabitCo/Features/ViewModel/WidgetViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  WidgetViewModel.swift
+//  HabitCo
+//
+//  Created by Geraldy Kumara on 09/05/24.
+//
+
+import Foundation
+
+@MainActor
+public final class WidgetViewModel: ObservableObject {
+    
+    @Published private(set) var subJournals: [(subJournal: SubJournalDB, habit: HabitDB?, pomodoro: PomodoroDB?)]? = nil
+
+    private let firebaseProvider: FirebaseAuthProvider
+    private let userManager: UserManager
+    
+    init() {
+        firebaseProvider = FirebaseAuthProvider()
+        userManager = UserManager.shared
+    }
+}
+
+public extension WidgetViewModel {
+    func getSubJournalToday() {
+        Task {
+            guard let userId = UserDefaultManager.userID else { return }
+            guard let subJournals = try await userManager.getSubJournals(userId: userId, from: Date().formattedDate(to: .fullMonthName), label: nil, isAscending: nil),
+                  !subJournals.isEmpty
+            else {
+                self.subJournals = nil
+                return
+            }
+            self.subJournals = try await fetchSubJournal(userId: userId, subJournals: subJournals)
+        }
+    }
+}
+
+private extension WidgetViewModel {
+    func fetchSubJournal(userId: String, subJournals: [SubJournalDB]) async throws -> [(subJournal: SubJournalDB, habit: HabitDB?, pomodoro: PomodoroDB?)] {
+        var localArray: [(subJournal: SubJournalDB, habit: HabitDB?, pomodoro: PomodoroDB?)] = []
+        for subJournal in subJournals {
+            if subJournal.subJournalType == .habit {
+                if let habitPomodoroId = try await userManager.getHabitDetail(userId: userId, habitId: subJournal.habitPomodoroId ?? "") {
+                    localArray.append((subJournal, habitPomodoroId, nil))
+                }
+            } else {
+                let habitPomodoroId = try await userManager.getPomodoroDetail(userId: userId, pomodoroId: subJournal.habitPomodoroId ?? "")
+                localArray.append((subJournal, nil, habitPomodoroId))
+            }
+        }
+        return localArray
+    }
+}
