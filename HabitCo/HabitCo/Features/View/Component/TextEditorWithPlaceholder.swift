4086f4ab2a4835b7f53123603e24725a29f61198
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2024-05-22 17:21:33 +0700

    fix: adjust the color of text editor (still need testing for ios 15 or below

--- HabitCo/HabitCo/Features/View/Component/TextEditorWithPlaceholder.swift
+++ HabitCo/HabitCo/Features/View/Component/TextEditorWithPlaceholder.swift
@@ -7,36 +7,58 @@
 
 import SwiftUI
 
+
 struct TextEditorWithPlaceholder: View {
     let placeholder: String
-     @Binding var text: String
-     
-     var body: some View {
-         ZStack(alignment: .leading) {
-             
-             
-             VStack {
-                 TextEditor(text: $text)
-                     .frame(minHeight: 150, maxHeight: 300)
-                     
-//                     .opacity(text.isEmpty ? 0.85 : 1)
-                 Spacer()
-             }
-             
-             if text.isEmpty {
+    @Binding var text: String
+
+    init(placeholder: String, text: Binding<String>) {
+        self.placeholder = placeholder
+        self._text = text
+    }
+
+    var body: some View {
+        ZStack(alignment: .leading) {
+
+
+            VStack {
+                TextEditor(text: $text)
+                    .frame(minHeight: 150, maxHeight: 300)
+                    .textEditorBackground(.getAppColor(.neutral3))
+                //                     .opacity(text.isEmpty ? 0.85 : 1)
+                Spacer()
+            }
+
+            if text.isEmpty {
                 VStack {
-                     Text(placeholder)
+                    Text(placeholder)
                         .foregroundColor(.getAppColor(.primary2))
-                         .padding(.top, 10)
-                         .padding(.leading, 3)
-                         
-                     Spacer()
-                 }
-             }
-         }
-     }
- }
+                        .padding(.top, 10)
+                        .padding(.leading, 3)
+
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+extension View {
+
+    func textEditorBackground(_ content: Color) -> some View {
+        if #available(iOS 16.0, *) {
+            return self.scrollContentBackground(.hidden)
+                .background(content)
+        } else {
+            print("ASKDM")
+            UITextView.appearance().backgroundColor = .clear
+            return self.background(content)
+        }
+    }
+}
 
 #Preview {
-    TextEditorWithPlaceholder(placeholder: "Insert...", text: .constant(""))
+//    TextEditorWithPlaceholder(placeholder: "Insert...", text: .constant(""))
+    TextEditor(text: .constant("HELLO WORLD"))
+        .textEditorBackground(.red)
 }
